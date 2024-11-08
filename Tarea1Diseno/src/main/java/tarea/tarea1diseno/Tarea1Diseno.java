cc05b8c655ad8e638dcee0367529ad055e34c83b
cimontesm <cimontes@espol.edu.ec>
2024-11-08 09:05:19 -0500

    Creacion proyecto

---
+++ Tarea1Diseno/src/main/java/tarea/tarea1diseno/Tarea1Diseno.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package tarea.tarea1diseno;
+
+/**
+ *
+ * @author CMONTES
+ */
+public class Tarea1Diseno {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
