39955e7b986795a8588ba971c39f2e9ba917108a
cimontesm <cimontes@espol.edu.ec>
2024-11-08 22:41:08 -0500

    clase usuario

--- Tarea1Diseno/src/main/java/tarea/tarea1diseno/Usuario.java
+++ Tarea1Diseno/src/main/java/tarea/tarea1diseno/Usuario.java
@@ -9,5 +9,62 @@ package tarea.tarea1diseno;
  * @author CMONTES
  */
 public class Usuario {
+    private int id;
+    private String nombre;
+    private String email;
+    private String contrasena;
+
+    public Usuario(int id, String nombre, String email, String contrasena) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+        this.contrasena = contrasena;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+    
+    public boolean iniciarSesion(){
+        //logica
+        return true;
+    }
+    
+    public boolean cerrarSesion(){
+        //logica
+        return false;
+    }
+    
+    public void registrarse(){
+        
+    }
     
 }
